20a893dd5127f90a94cfff5f511c6e1af0aab3b2
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2021-04-18 19:20:14 +0545

    Set first player to X and second Player to O

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include<windows.h>
 using namespace std;
 
-
+//stores the position and symbol of the players
 struct Coordinates{
     int position;
     char symbol;
@@ -19,6 +19,7 @@ struct Coordinates{
 class Player;
 class Game{
     static Coordinates c[9];
+    //store how many time players position was taken
     static int play_count;
     public:
     void set_comp_pos(Player&);
@@ -40,18 +41,16 @@ class Player{
     Player(){
         is_winner = false;
         is_comp = false;
+        is_my_turn = true;
     }
     void input_player_detail();
-    void display_detail(){
-        cout<<"Name:"<<name<<'\n';
-        cout<<"Symbol:"<<symbol<<'\n';
-    }
     void  make_comp(){
         is_comp = !is_comp;
     }
 };
 
 void Game::display() const{
+    system("CLS");
     cout<<"This is Tic Tac Toe By Abhilekh Gautam\n";
     cout<<"Enter any Key to continue to game\n";
     getch();
@@ -59,6 +58,7 @@ void Game::display() const{
 }
 
 void Game::displayGraphics(Player& p1,Player& p2){
+  //take position for maximum of 9 times.
     for(int i = 0 ; i < 9 ; i++)
     {
         play_count++;
@@ -98,10 +98,11 @@ void Game::displayGraphics(Player& p1,Player& p2){
     //assigning symbol to the designated position
     c[choice-1].symbol = curr_player.symbol;
     }
+    //Setting up computer's Move
     else{
         set_comp_pos(curr_player);
     }
-
+    //Check for winner
     checkWinner(curr_player);
     system("CLS");
 
@@ -110,6 +111,7 @@ void Game::displayGraphics(Player& p1,Player& p2){
             cout<<"Computer won\n";
         }
         else{
+          //display positions after victory
             cout<<curr_player.name<<" is winner\n";
         }
         cout<<c[0].symbol<<'\t'<<"|"<<c[1].symbol<<'\t'<<"|"<<c[2].symbol<<'\n';
@@ -124,6 +126,7 @@ void Game::displayGraphics(Player& p1,Player& p2){
     }
     if(play_count == 9){
         if(!curr_player.is_winner){
+          //display position after game draws
             cout<<"Game drawn\n";
             cout<<c[0].symbol<<'\t'<<"|"<<c[1].symbol<<'\t'<<"|"<<c[2].symbol<<'\n';
             cout<<"\t"<<"|"<<"\t"<<"|"<<'\n';
@@ -144,14 +147,17 @@ void Game::displayGraphics(Player& p1,Player& p2){
    }
 }
 
+//Find the best move for Computer.
 void Game::set_comp_pos(Player& P){
   cout<<"Computers Turn...\n";
   cout<<"Thinking...\n";
   Sleep(1800);
 
+//place computers symbol in between opponent's symbol (X   O   X)
     if((c[0].symbol ==' ' || c[1].symbol == ' ' || c[2].symbol == ' ') && (((c[0].symbol == c[2].symbol) && c[0].symbol != ' ') || ((c[0].symbol == c[1].symbol) && (c[0].symbol != ' ')) || ((c[1].symbol == c[2].symbol) && c[1].symbol!=' '))){
        if(c[0].symbol == ' '){
           if(c[1].symbol != P.symbol){
+            //check for move which lead to victory
             check_comp_pos(P);
           }
           else{
@@ -373,15 +379,15 @@ void Game::set_comp_pos(Player& P){
    else{
        
        //target bottom left corner
-        if(c[6].symbol == ' '){
+        if(c[6].symbol == ' ' && c[4].symbol !=' '){
             c[6].symbol = P.symbol;
        }
        //target top right corner
-       else if(c[2].symbol == ' '){
+       else if(c[2].symbol == ' '&& c[4].symbol !=' '){
            c[2].symbol = P.symbol;
        }
        //top left
-       else if(c[0].symbol == ' '){
+       else if(c[0].symbol == ' '&& c[4].symbol !=' '){
            c[0].symbol = P.symbol;
        }
        //Always target the center first
@@ -389,6 +395,14 @@ void Game::set_comp_pos(Player& P){
            c[4].symbol = P.symbol;
        }
 
+       else if(c[6].symbol !=' ' && c[6].symbol != P.symbol && c[7].symbol == ' '){
+         c[7].symbol = P.symbol;
+       }
+
+       else if(c[1].symbol !=' ' && c[1].symbol != P.symbol && c[2].symbol == ' '){
+         c[2].symbol = P.symbol;
+       }
+       //if no condition satisfies randomly choose a empty position
        else{
            for(int i = 8 ; i >= 0 ; i--){
                if(c[i].symbol ==' '){
@@ -487,7 +501,7 @@ void Game::check_comp_pos(Player& P){
        
 }
 
-
+//check if the winner already exist or not
 void Game::checkWinner(Player& P){
  
    if(c[0].symbol !=' ' && c[0].symbol == c[1].symbol && c[1].symbol == c[2].symbol){
@@ -517,10 +531,13 @@ void Game::checkWinner(Player& P){
 }
 
 void Player::input_player_detail(){
+  //input only if player is not a computer
 if(!is_comp){
+  //no need to ask for the symbol of the second player
  if(count == 0)
  {
     cout<<"Enter Player1 Name\n";
+    //cin doesn't take characters after space
     cin>>name;
       cout<<"Choose a symbol 'X' or 'O'\n";
     char ch = getch();
@@ -530,7 +547,6 @@ if(!is_comp){
         cout<<"X selected as symbol for "<<this->name<<'\n';
         count++;
         sym = symbol;
-        is_my_turn = true;
     }
     else if(ch == 'O'||ch =='o')
     {
@@ -538,20 +554,23 @@ if(!is_comp){
         cout<<"O selected as symbol for "<<this->name<<'\n';
         count++;
         sym = symbol;
-    }
 
+        is_my_turn = !is_my_turn;
+
+    }
+    //Automatically select 'X' for invalid selection by user
     else{
         cout<<"Invalid Input\n";
         cout<<"X selected as symbol for "<<this->name<<" by default"<<'\n';
+        //input count increased
         count++;
         symbol= 'X';
         sym = symbol;
-
-        is_my_turn = true;
        }
 
    }
 
+//only take the players detail is game mode is multiplayer
     else
     {
         cout<<"Enter Player2 Name\n";
@@ -560,6 +579,8 @@ if(!is_comp){
         {
          symbol = 'O';
          cout<<"O is the symbol for "<<this->name<<'\n';
+         //Not your turn if you choose 'O';
+         is_my_turn = !is_my_turn;
         }
 
         else
@@ -573,11 +594,14 @@ if(!is_comp){
 Sleep(1000);
 system("CLS");
  }
+ //automatically select symbol for second player
  else
  {
      if(sym == 'X'){
          symbol = 'O';
          cout<<"O is the symbol for Computer\n";
+
+          is_my_turn = !is_my_turn;
         }
 
         else{
@@ -592,8 +616,6 @@ system("CLS");
 void Game::input(Player& p1, Player& p2){
 p1.input_player_detail();
 p2.input_player_detail();
-p1.display_detail();
-p2.display_detail();
 system("CLS");
 displayGraphics(p1,p2);
 }
@@ -603,22 +625,25 @@ char Player::sym;
 Coordinates Game::c[9];
 int Game::play_count;
 
+
 int main(){
+     Game G;
+     G.display();
     int choice;
     cout<<"Select Game Mode\n";
     cout<<"1.Single Player(vs Com)\n";
     cout<<"2.Multiplayer\n";
     cin>>choice;
+    //Force the choice among 1 and 2
     while(choice!=1 && choice!=2){
         cout<<"Choose In Between 1 and 2\n";
         cin>>choice;
     }
-    Game G;
     Player p1,p2;
     if(choice == 1){
+      //one player is computer
         p2.make_comp();
     }
-    G.display();
     G.input(p1,p2);
     return 0;
 }
